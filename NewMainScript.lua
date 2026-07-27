6de5cb68cfb1e4ecf251d5efcc00cc25c8f3db38
Zynthire <maximearsenault7@gmail.com>
2026-07-27 14:22:26 -0400

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -87,7 +87,7 @@ end
 
 if not shared.VapeDeveloper then
 	local _, subbed = pcall(function()
-		return game:HttpGet('https://github.com/poopparty/poopparty')
+		return game:HttpGet('https://github.com/Zynthire/Frostware')
 	end)
 
 	local commit = 'main'
